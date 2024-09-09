465c428640d680fabfb001205afd4bc218f94f53
ivanv-lab <ivanvozisov33@yandex.ru>
2024-09-09 11:51:44 +0300

    Нужно доделать прогу, что бы хотя бы полный список папок и файлов получить

--- Program.cs
+++ Program.cs
@@ -1,19 +1,43 @@
-﻿namespace YandexDiskAPI1
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace YandexDiskAPI1
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            var authManaher=new AuthManager();
-            var accessToken=await authManaher.GetAccessTokenAsync();
 
-            var diskApi=new DiskFileManager(accessToken);
+            var ouathToken = "y0_AgAAAAAjNsyhAAxoAwAAAAEQYp3XAAB65Pto3Q9Hrr_OPnsmRHacs_5zug";
 
-            var files = await diskApi.ListFilesResponse("");
-            foreach (var file in files.Embedded.Items)
+            await GetYandexDiskFiles(ouathToken);
+        }
+
+        private static async Task GetYandexDiskFiles(string token)
+        {
+            using (var httpClient = new HttpClient())
             {
-                Console.WriteLine(file.Name);
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
+
+                var response = await httpClient.GetAsync("https://cloud-api.yandex.net/v1/disk/resources/files");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(content);
+                    foreach (var item in json["items"])
+                    {
+                        string name = item["name"].ToString();
+                        Console.WriteLine(name);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: {response.StatusCode}");
+                    string errorContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(errorContent);
+                }
             }
         }
     }
