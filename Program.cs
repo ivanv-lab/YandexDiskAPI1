fca4d05b975c214437d07c727cd8e2f1abd0101c
ivanv-lab <ivanvozisov33@yandex.ru>
2024-09-11 11:28:07 +0300

    Done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.IO;
 using System.Net.Http.Headers;
 
 namespace YandexDiskAPI1
@@ -13,60 +14,12 @@ namespace YandexDiskAPI1
             var authManager = new AuthManager();
             var oauthToken = await authManager.GetToken();
 
-            // oauthToken = "y0_AgAAAAAjNsyhAAxoAwAAAAEQYp3XAAB65Pto3Q9Hrr_OPnsmRHacs_5zug";
-            var stopwatch = Stopwatch.StartNew();
             Console.WriteLine("Подождите");
-            await GetYandexDiskFiles(oauthToken);
+            var manager=new YandexFileManager(oauthToken);
 
-            Console.WriteLine("ПАПКИ НА УДАЛЕНИЕ::::::::::::::::::::::::::::::::::");
-            foreach(var path in foldersToDelete)
-            {
-                Console.WriteLine(path);
-            }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.ElapsedMilliseconds);
-        }
-        public static List<string>foldersToDelete = new List<string>();
-        private static async Task GetYandexDiskFiles(string token,string path="/")
-        {
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
+            await manager.Run();
 
-                var response = await httpClient.GetAsync("https://cloud-api.yandex.net/v1/disk" +
-                    $"/resources?path={path}");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = await response.Content.ReadAsStringAsync();
-                    JObject json = JObject.Parse(content);
-
-                    List<string> paths = new List<string>();
-
-                    foreach (var item in json["_embedded"]["items"])
-                    {
-                        if (item["type"].ToString() == "dir")
-                        {
-                            paths.Add(item["path"].ToString());
-                        }
-                    }
-                    if (json["_embedded"]["items"].Count() == 0)
-                    {
-                        foldersToDelete.Add(path);
-                        return;
-                    }
-                    var tasks = paths.Select(async p => await
-                    GetYandexDiskFiles(token, p)).ToList();
-                    await Task.WhenAll(tasks);
-                    //foreach (var pat in paths)
-                    //{
-                        //await GetYandexDiskFiles(token, pat);
-
-                        //Parallel.Invoke(async () =>
-                        //await GetYandexDiskFiles(token, pat));
-                    //}
-                }
-            }
+            Console.WriteLine("Удаление завершено");
         }
     }
 }
---
+++ YandexFileManager.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YandexDiskAPI1
+{
+    public class YandexFileManager
+    {
+        public string token { get; set; }
+        private List<string> foldersToDelete=new List<string>();
+        public YandexFileManager(string token)
+        {
+            this.token = token;
+        }
+        public async Task Run()
+        {
+            foldersToDelete.Clear();
+            await GetYandexDiskFiles(token);
+            if (foldersToDelete.Count == 0)
+            {
+                Console.WriteLine("Нет пустых папок");
+                return;
+            }
+            await DeleteFolders(token);
+            if (foldersToDelete.Count > 0)
+                await Run();
+        }
+        private async Task GetYandexDiskFiles(string token, string path = "/")
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
+
+                var response = await httpClient.GetAsync("https://cloud-api.yandex.net/v1/disk" +
+                    $"/resources?path={path}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    JObject json = JObject.Parse(content);
+
+                    List<string> paths = new List<string>();
+
+                    foreach (var item in json["_embedded"]["items"])
+                    {
+                        if (item["type"].ToString() == "dir")
+                        {
+                            paths.Add(item["path"].ToString());
+                        }
+                    }
+                    if (json["_embedded"]["items"].Count() == 0)
+                    {
+                        foldersToDelete.Add(path);
+                        return;
+                    }
+                    var tasks = paths.Select(async p => await
+                    GetYandexDiskFiles(token, p)).ToList();
+                    await Task.WhenAll(tasks);
+                }
+            }
+        }
+        private async Task DeleteFolders(string token)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("OAuth", token);
+
+                string url = "https://cloud-api.yandex.net/v1/disk" +
+                    "/resources?path=";
+
+                var tasks = foldersToDelete.Select(async p =>
+                await httpClient.DeleteAsync(url + p)).ToList();
+                await Task.WhenAll(tasks);
+                //foreach (string path in foldersToDelete)
+                //{
+                //    string urlToFolder = url + path;
+                //    var response = await httpClient.DeleteAsync(urlToFolder);
+                //    if (response.IsSuccessStatusCode)
+                //    {
+                //        Console.WriteLine($"{path} успешно удален");
+                //    }
+                //}
+            }
+        }
+    }
+}
